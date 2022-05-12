d3ae831496dab966784e539eb4dfa57229be5bae
huiiii0 <103632496+huiiii0@users.noreply.github.com>
2022-05-12 17:13:23 +0900

    Add files via upload

---
+++ react_pot_2/js/nav_slideToggle.js
@@ -0,0 +1,14 @@
+﻿
+$(document).ready(function () {
+			$(".a").click(function () {
+				$(".b").slideToggle();
+			});
+		});
+		$(window).resize(function () {
+			if (window.innerWidth >= 1100) {
+				$(".b").show();
+			}
+			if (window.innerWidth <= 1099) {
+				$(".b").hide();
+			}
+		});
